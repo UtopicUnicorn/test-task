6f10dd966a588fc403022ff1805f6ef2dbeb3a6a
UtopicUnicorn <medvedevtimofei@gmail.com>
2021-12-29 19:09:38 +0700

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTableModule } from "@angular/material/table";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersTableComponent } from './users-table/users-table.component';
+import { HeaderComponent } from './header/header.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {TableServiceService} from "./services/table-service.service";
+import {baseURL} from "./baseurl";
+import { ProcessHttpmsgService } from "./services/process-httpmsg.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersTableComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatTableModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    TableServiceService,
+    ProcessHttpmsgService,
+    {provide: 'baseURL', useValue: baseURL}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
