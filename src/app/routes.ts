6cafc68091fccc1716f5fc0c828bcab67370848f
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-01-10 00:08:14 +0700

    added new paths

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from "@angular/router";
 
 import {UsersTableComponent} from "./users-table/users-table.component";
+import {UserEditorComponent} from "./user-editor/user-editor.component";
+import {LoginComponent} from "./login/login.component";
+import {RegistrationPageComponent} from "./registration-page/registration-page.component";
 
 export const routes: Routes = [
   {path:'table', component: UsersTableComponent},
-  {path:'', redirectTo: '/table', pathMatch:'full'}
+  {path:'user-detail/:id', component: UserEditorComponent},
+  {path:'login', component:LoginComponent},
+  {path:'registration', component:RegistrationPageComponent},
+  {path:'', redirectTo: '/login', pathMatch:'full'}
 ]
