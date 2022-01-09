aa061c616abbc30b813d4831bd53168d6ea4dd20
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-01-10 00:11:54 +0700

    added functions to edit and delete user

--- src/app/users-table/users-table.component.ts
+++ src/app/users-table/users-table.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {userEntity} from "./userEntity";
 import {TableServiceService} from "../services/table-service.service";
 import {SourcesEntity} from "./sourcesEntity";
-
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-users-table',
   templateUrl: './users-table.component.html',
@@ -11,21 +11,39 @@ import {SourcesEntity} from "./sourcesEntity";
 export class UsersTableComponent implements OnInit {
   users!: userEntity[];
   sources!:SourcesEntity[];
+  errMessage!: string;
 
   userTableDisplayedColumns!: string[];
   sourceTableDisplayedColumns!: string[];
 
-  constructor(private tableService: TableServiceService){ }
+  constructor(private tableService: TableServiceService, private router:Router){ }
 
   ngOnInit(): void {
     this.tableService.getUsers().subscribe((data:userEntity[])=>this.users = data);
     this.tableService.getSourcese().subscribe((data:SourcesEntity[])=>this.sources = data);
 
-    this.userTableDisplayedColumns = ['email','first_name', 'last_name', 'avatar'];
+    this.userTableDisplayedColumns = ['avatar', 'email','first_name', 'last_name','action'];
     this.sourceTableDisplayedColumns = ['name','year', 'color', 'pantone_value'];
 
+  }
+
+  userDetails(user: userEntity) : void{
+    console.log('edit');
+    console.log(user);
+    let id: number = +user.id;
+    this.router.navigate(['user-detail/', id]);
+  }
+
+  delete(user: userEntity) : void{
+    this.users = this.users.filter(u=>u!==user);
+    this.tableService.deleteUser(user).subscribe();
+  }
+
+  onValueChanged() : void{
 
   }
+
+
 }
 
 
