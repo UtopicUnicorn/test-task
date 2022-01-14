4fcd4e30f95f49ca5ebb54a761057b0eff76d30a
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-01-14 15:26:51 +0700

    added token check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private auth: AuthService) {
+  }
   title = 'test-task';
+
+  ngOnInit() {
+    const potToken = localStorage.getItem('token');
+    if(potToken!==null){
+      this.auth.setToken(potToken);
+    }
+  }
 }
