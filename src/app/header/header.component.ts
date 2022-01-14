2404f5459524f8fed3852c351cfa678019cfb063
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-01-14 15:29:56 +0700

    finished header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  logout(): void{
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
 }
