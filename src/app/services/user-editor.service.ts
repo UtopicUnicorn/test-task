5e9deab452db86356607783eaf3ce157ff8e768c
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-01-14 15:35:47 +0700

    removed unused code

--- src/app/services/user-editor.service.ts
+++ src/app/services/user-editor.service.ts
@@ -1,8 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {baseURL} from "../baseurl";
-import {ProcessHttpmsgService} from "./process-httpmsg.service";
-import {catchError, Observable} from "rxjs";
+import {Observable} from "rxjs";
 import {userEntity} from "../users-table/userEntity";
 import {map} from "rxjs/operators";
 
@@ -11,8 +10,7 @@ import {map} from "rxjs/operators";
 })
 export class UserEditorService {
 
-  constructor(private http: HttpClient,
-              private processHTTPMsgService: ProcessHttpmsgService) { }
+  constructor(private http: HttpClient) { }
 
   getUser(id: string):Observable<userEntity>{
     return this.http.get<userEntity>(baseURL + `/${id}`)
@@ -20,15 +18,6 @@ export class UserEditorService {
         return data['data']})
       )
   };
-  //     return user.map(function (user:any):userEntity{
-  //       return new userEntity(user.id, user.email, user.first_name, user.last_name, user.avatar);
-  //     });
-  //   }),catchError(this.processHTTPMsgService.handleError));
-  // }
-  // getUser(id: string):Observable<userEntity>{
-  //   return this.http.get<userEntity>(baseURL + `/${id}`).pipe(catchError(this.processHTTPMsgService.handleError));
-  //
-  // }
 
   updateForm(id:string,sendForm:userEntity): Observable<any>{
     const httpOptions = {
@@ -36,8 +25,7 @@ export class UserEditorService {
         'Content-Type': 'application/json'
       })
     };
-    return this.http.put<userEntity>(baseURL + `/${id}`, sendForm, httpOptions)
-      .pipe(catchError(this.processHTTPMsgService.handleError));
+    return this.http.put<userEntity>(baseURL + `/${id}`, sendForm, httpOptions);
 
   }
 
