f2f6ea1f98ce37032cc3535a7af40388a8431e64
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-01-14 15:31:38 +0700

    added route protection

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,10 +4,11 @@ import {UsersTableComponent} from "./users-table/users-table.component";
 import {UserEditorComponent} from "./user-editor/user-editor.component";
 import {LoginComponent} from "./login/login.component";
 import {RegistrationPageComponent} from "./registration-page/registration-page.component";
+import {AuthGuard} from "./auth.guard";
 
 export const routes: Routes = [
-  {path:'table', component: UsersTableComponent},
-  {path:'user-detail/:id', component: UserEditorComponent},
+  {path:'table', component: UsersTableComponent, canActivate:[AuthGuard]},
+  {path:'user-detail/:id', component: UserEditorComponent,canActivate:[AuthGuard]},
   {path:'login', component:LoginComponent},
   {path:'registration', component:RegistrationPageComponent},
   {path:'', redirectTo: '/login', pathMatch:'full'}
